0192e116f7b17469bb3ad9fd9e0417e74dd78a97
Mohini <54019867+mohiniawadhiya@users.noreply.github.com>
2023-09-09 19:04:36 +0530

    Add files via upload

---
+++ filereader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+             Console.Clear();
+             StreamReader reader = new StreamReader(@"C:\Practice\ApplicationLogs.txt");
+
+             string str = String.Empty;
+
+             while(reader.EndOfStream == false)
+             {
+                 str = reader.ReadLine();
+                 Console.WriteLine(str);
+             }
+
+             reader.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ metadata.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+           static void Main()
+        {
+            // Declaring and initializing object of Type
+            Type objType = typeof(Student);
+
+            // try-catch block for handling Exception
+            try
+            {
+
+                // Getting array of Method by
+                // using GetMethods() Method
+                MethodInfo[] info = objType.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+
+                // Display the Result
+                Console.WriteLine("Methods of current type is as Follow: ");
+                for (int i = 0; i < info.Length; i++)
+                    Console.WriteLine(" {0}", info[i]);
+            }
+
+            // catch ArgumentNullException here
+            catch (ArgumentNullException e)
+            {
+                Console.Write("name is null.");
+                Console.Write("Exception Thrown: ");
+                Console.Write("{0}", e.GetType(), e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        public string Subject { get; set; }
+
+        public void printName()
+        {
+            Console.WriteLine(" " + Name + " ");
+        }
+
+        public void printSubject()
+        {
+            Console.WriteLine(" " + Subject + " ");
+        }
+
+        public void printRoll()
+        {
+            Console.WriteLine(" " + RollNo + " ");
+        }
+    }
+}
